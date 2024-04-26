edce237131a93fabdd33905da5180c44b106fe5e
Aaron Stanek <amr.stanek@gmail.com>
2024-04-26 16:34:39 -0700

    basic tests pass

--- index.test.ts
+++ index.test.ts
@@ -4,34 +4,34 @@ let passedTests = 0;
 let failedTests = 0;
 
 const expect = (
-  errorMessage: string,
-  resolver: () => number | null,
-  targetValue: number | null,
+    errorMessage: string,
+    resolver: () => number | null,
+    targetValue: number | null,
 ) => {
-  let result;
-  try {
-    result = resolver();
-  } catch (e: any) {
-    failedTests += 1;
-    console.log(`Test threw an exception: ${errorMessage}`);
-    return;
-  }
-  if (result === targetValue) {
-    passedTests += 1;
-  } else {
-    failedTests += 1;
-    console.log(
-      `Test failed: ${errorMessage}\nExpected: ${targetValue}\nReceived: ${result}\n`,
-    );
-  }
+    let result;
+    try {
+        result = resolver();
+    } catch (e: any) {
+        failedTests += 1;
+        console.log(`Test threw an exception: ${errorMessage}`);
+        return;
+    }
+    if (result === targetValue) {
+        passedTests += 1;
+    } else {
+        failedTests += 1;
+        console.log(
+            `Test failed: ${errorMessage}\nExpected: ${targetValue}\nReceived: ${result}\n`,
+        );
+    }
 };
 
 expect("distance between empty strings is zero", () => levenshtein("", ""), 0);
 
 expect(
-  "distance between identical strings is zero",
-  () => levenshtein("a", "a"),
-  0,
+    "distance between identical strings is zero",
+    () => levenshtein("a", "a"),
+    0,
 );
 
 expect("one insertion has cost 1", () => levenshtein("", "a"), 1);
--- index.ts
+++ index.ts
@@ -1,30 +1,106 @@
 export type LevenshteinOptions = {
-  maxCost?: number;
-  insertionCost?: number;
-  deletionCost?: number;
-  replacementCost?: number;
+    maxCost?: number;
+    insertionCost?: number;
+    deletionCost?: number;
+    replacementCost?: number;
 };
 
 type LevenshteinImplConfig = {
-  a: string;
-  b: string;
-  maxCost: number;
-  insertionCost: number;
-  deletionCost: number;
-  replacementCost: number;
+    a: string;
+    b: string;
+    maxCost: number;
+    insertionCost: number;
+    deletionCost: number;
+    replacementCost: number;
 };
 
 const levenshteinImpl = (
-  config: LevenshteinImplConfig,
-  aIndex: number,
-  bIndex: number,
-  cost: number,
+    config: LevenshteinImplConfig,
+    aIndex: number,
+    bIndex: number,
+    cost: number,
 ): number | null => {
-  return null;
+    while (aIndex < config.a.length && bIndex < config.b.length) {
+        const aChar = config.a.codePointAt(aIndex) as number;
+        const bChar = config.b.codePointAt(bIndex) as number;
+        if (aChar !== bChar) break;
+        const increment = 1 + +(aChar > 0xffff);
+        aIndex += increment;
+        bIndex += increment;
+    }
+    if (aIndex >= config.a.length && bIndex >= config.b.length) return cost;
+    let bestResult: number | null = null;
+    if (aIndex < config.a.length) {
+        bestResult = levenshteinImpl(
+            config,
+            aIndex + 1 + +((config.a.codePointAt(aIndex) as number) > 0xffff),
+            bIndex,
+            cost + config.insertionCost,
+        );
+    }
+    if (bIndex < config.b.length) {
+        let result = levenshteinImpl(
+            config,
+            aIndex,
+            bIndex + 1 + +((config.b.codePointAt(bIndex) as number) > 0xffff),
+            cost + config.deletionCost,
+        );
+        if (bestResult === null || (result !== null && result < bestResult))
+            bestResult = result;
+        if (aIndex < config.a.length) {
+            result = levenshteinImpl(
+                config,
+                aIndex +
+                    1 +
+                    +((config.a.codePointAt(aIndex) as number) > 0xffff),
+                bIndex +
+                    1 +
+                    +((config.b.codePointAt(bIndex) as number) > 0xffff),
+                cost + config.replacementCost,
+            );
+            if (bestResult === null || (result !== null && result < bestResult))
+                bestResult = result;
+        }
+    }
+    return bestResult;
+};
+
+const isUndefinedOrPositive = (x: number | undefined): boolean => {
+    if (x === undefined) return true;
+    if (!isFinite(x)) return false;
+    return x >= 0;
 };
 
 export const levenshtein = (
-  a: string,
-  b: string,
-  options?: LevenshteinOptions,
-) => 5;
+    a: string,
+    b: string,
+    options?: LevenshteinOptions,
+) => {
+    if (!isUndefinedOrPositive(options?.maxCost))
+        throw new Error(`Invalid value for maxCost: ${options?.maxCost}`);
+    if (!isUndefinedOrPositive(options?.insertionCost))
+        throw new Error(
+            `Invalid value for insertionCost: ${options?.insertionCost}`,
+        );
+    if (!isUndefinedOrPositive(options?.deletionCost))
+        throw new Error(
+            `Invalid value for deletionCost: ${options?.deletionCost}`,
+        );
+    if (!isUndefinedOrPositive(options?.replacementCost))
+        throw new Error(
+            `Invalid value for replacementCost: ${options?.replacementCost}`,
+        );
+    return levenshteinImpl(
+        {
+            a,
+            b,
+            maxCost: options?.maxCost ?? -1,
+            insertionCost: options?.insertionCost ?? 1,
+            deletionCost: options?.deletionCost ?? 1,
+            replacementCost: options?.replacementCost ?? 1,
+        },
+        0,
+        0,
+        0,
+    );
+};

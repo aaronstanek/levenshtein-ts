c4f61d0f116a07050711c305aba66fc47a84c453
Aaron Stanek <amr.stanek@gmail.com>
2024-04-26 12:11:46 -0700

    set up repo

---
+++ index.test.ts
@@ -0,0 +1,1 @@
+import { levenshtein } from "./index";
---
+++ index.ts
@@ -0,0 +1,30 @@
+export type LevenshteinOptions = {
+  maxCost?: number;
+  insertionCost?: number;
+  deletionCost?: number;
+  replacementCost?: number;
+};
+
+type LevenshteinImplConfig = {
+  a: string;
+  b: string;
+  maxCost: number;
+  insertionCost: number;
+  deletionCost: number;
+  replacementCost: number;
+};
+
+const levenshteinImpl = (
+  config: LevenshteinImplConfig,
+  aIndex: number,
+  bIndex: number,
+  cost: number,
+): number | null => {
+  return null;
+};
+
+export const levenshtein = (
+  a: string,
+  b: string,
+  options: LevenshteinOptions,
+) => 5;
